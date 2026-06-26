fd2f14fe5f694407c3b3de0bc21eaeb8f8d2a8dd
toan17032006 <toanboy17032006@gmail.com>
2026-06-26 16:54:40 +0700

    kieudulieucoban

---
+++ laptrinhjava/HinhChuNhat.java
@@ -0,0 +1,21 @@
+package laptrinhjava;
+
+
+import java.util.Scanner;
+
+public class HinhChuNhat {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int dai = sc.nextInt();
+        int rong = sc.nextInt();
+        if(dai<=0 || rong<= 0){
+            System.out.println(0);
+        }
+        else{
+            int chuvi = 2*(dai + rong);
+            int dientich = dai * rong;
+            System.out.println(chuvi +" " + dientich);
+        }
+        sc.close();
+    }
+}
